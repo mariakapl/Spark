d82175f8e2ac43f2f7fe81a5661175e452ef639d
TomerKr <tkrworkmail@gmail.com>
2015-07-29 15:38:09 +0300

    Getting ionic to work
    Trying ionic stuff

--- MediCycle/www/js/app.js
+++ MediCycle/www/js/app.js
@@ -48,14 +48,33 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         controller: 'DashCtrl'
       }
     }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
+  })  .state('tab.medicine', {
+      url: '/medicinelist',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+        'tab-dash': {
+          templateUrl: 'templates/tab-medicine.html',
+          controller: 'MedicineCtrl'
+        }
+      }
+    })
+  
+  
+    .state('tab.donation', {
+      url: '/donationlist',
+      views: {
+        'tab-dash': {
+          templateUrl: 'templates/tab-donation.html',
+    //      controller: 'MedicineCtrl'
+        }
+      }
+    })
+	
+  .state('tab.info', {
+      url: '/info',
+      views: {
+        'tab-info': {
+          templateUrl: 'templates/tab-info.html',
+      //    controller: 'MedicineCtrl'
         }
       }
     })
@@ -64,20 +83,10 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       views: {
         'tab-chats': {
           templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+          //controller: 'ChatDetailCtrl'
         }
       }
     })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
--- MediCycle/www/js/services.js
+++ MediCycle/www/js/services.js
@@ -1,50 +1,31 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
+.factory('Medicines', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var chats = [{
+  var meds = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+    name: 'Coldex',
+    exp: '1/1/1000'
   }];
-
   return {
     all: function() {
-      return chats;
+      return meds;
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
+    remove: function(med) {
+      meds.splice(meds.indexOf(med), 1);
     },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    get: function(medId) {
+      for (var i = 0; i < meds.length; i++) {
+        if (meds[i].id === parseInt(medId)) {
+          return meds[i];
         }
       }
       return null;
-    }
+    },
+	add: function(med){
+		meds.push(med);
+	}
   };
 });
